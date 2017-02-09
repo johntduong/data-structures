16aad46f4006f1ffb002da5be8cd3cbcd2bbd200
johntduong <johntduong@yahoo.com>
2017-02-09 10:13:48 -0800

    Finished psudo classical repo

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.head = 0;
+  this.tail = 0;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.tail] = value;
+  this.tail++;
+};
+
+Queue.prototype.dequeue = function() {
+  var value = this.storage[this.head];
+  delete this.storage[this.head];
+  this.head++;
+  return value;
 };
 
 
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.number = 0;
+  this.storage = {};
 };
 
 
+Stack.prototype.push = function(value) {
+  this.storage[this.number] = value;
+  this.number++;
+};
+
+Stack.prototype.pop = function() {
+  this.number--;
+  if (this.number < 0) {
+    this.number = 0;
+  }
+  return this.storage[this.number];
+};
+
+Stack.prototype.size = function() {
+  return this.number;
+};
